8c026eeee9573a93cb77c0f4398dc23c89083c67
SimonCzEdu <simonczedu@gmail.com>
2024-04-09 16:37:20 +0100

    Script: add functionality to settings screen

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,22 @@
+// Settings Screen functions
+/**
+ * Function for opening and closing settings screen. By default it will be closed and when player presses the icon it will open.
+ */
+document.getElementById(`settingsBtn`).addEventListener('click', openSettings);
+
+function openSettings() {
+
+    const settingsSwitch = document.getElementById(`settingsCheck`);
+
+    if (settingsSwitch.checked) {
+        document.getElementById(`settings`).style.display = `block`;
+    }
+    else {
+        document.getElementById(`settings`).style.display = `none`;
+    }
+}
+
+
 // Inventory Screen functions
 /**
  * Function for opening and closing inventory screen. By default it will be closed and when player presses the icon it will open.
@@ -24,6 +43,16 @@ function openInv() {
 document.getElementById(`attack`).addEventListener(`click`, attack);
 
 function attack() {
+    
+    //Dice roll for the attack chance value without modifiers
     const attackRoll = Math.floor(Math.random() * 20 + 1)
     console.log(attackRoll);
+
+    // Check if player succeeded in landing the hit
+    if (attackRoll >= 12) {
+        console.log(`Your hit lands!`)    
+    }
+    else {
+        console.log(`Oh no! You missed!`)
+    }
 }
