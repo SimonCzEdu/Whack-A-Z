f1a5a4743f23f5bd9ecb0b9e52de90f892d47958
SimonCzEdu <simonczedu@gmail.com>
2024-04-09 14:58:18 +0100

    Script: adjust the script for default behavior

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
 // Inventory Screen functions
 /**
- * Function for opening and closing inventory screen
+ * Function for opening and closing inventory screen. By default it will be closed and when player presses the icon it will open.
  */
 document.getElementById(`invBtn`).addEventListener('click', openInv);
 
@@ -9,9 +9,9 @@ function openInv() {
     const invSwitch = document.getElementById(`invCheck`);
 
     if (invSwitch.checked) {
-        document.getElementById(`inv`).style.display = `none`;
+        document.getElementById(`inv`).style.display = `flex`;
     }
     else {
-        document.getElementById(`inv`).style.display = `flex`;
+        document.getElementById(`inv`).style.display = `none`;
     }
 }
