85394a2ddb1c96e0c392102823a6e25666252e9a
SimonCzEdu <simonczedu@gmail.com>
2024-04-10 19:38:40 +0100

    Script: move eventListeners & add comments

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,6 @@
 /**
  * Function for opening and closing settings screen. By default it will be closed and when player presses the icon it will open.
  */
-document.getElementById(`settingsBtn`).addEventListener('click', openSettings);
 
 function openSettings() {
 
@@ -15,14 +14,13 @@ function openSettings() {
         document.getElementById(`settings`).style.display = `none`;
     }
 }
-
+// Even listener for openSettings()
+document.getElementById(`settingsBtn`).addEventListener('click', openSettings);
 
 // Inventory Screen functions
 /**
  * Function for opening and closing inventory screen. By default it will be closed and when player presses the icon it will open.
  */
-document.getElementById(`invBtn`).addEventListener('click', openInv);
-
 function openInv() {
 
     const invSwitch = document.getElementById(`invCheck`);
@@ -34,29 +32,37 @@ function openInv() {
         document.getElementById(`inv`).style.display = `none`;
     }
 }
-
+// Even listener for openInv()
+document.getElementById(`invBtn`).addEventListener('click', openInv);
 
 // Actions functions
-/**
- * 
- */
+
+// Call attack() function when players clicks on #attack div
 document.getElementById(`attack`).addEventListener(`click`, attack);
-// Current Zombie health - must me declared with let as we will later on assign new values to it
+/* Current Zombie health - must me declared with 'let' as we will later assign new values to it.
+Must also be declared before we call the function.*/
 let currentZHealth = document.getElementById('zHealthIndicator').style.width = `100%`;
+// Now we need to make it an actual number
 currentZHealth = parseInt(currentZHealth);
 console.log(currentZHealth);
 
+// Attack Action Function 
+/**
+ * attack() function decides what happens when player clicks on Attack button
+ */
 function attack() {
 
-    //Dice roll for the attack chance value without modifiers
+    /**
+     * Dice roll for the attack chance value without modifiers is random number 1-20
+     */
     const attackRoll = Math.floor(Math.random() * 20 + 1)
     console.log(attackRoll);
 
-    // Check if player succeeded in landing the hit
-    if (attackRoll >= 12) {
+    // Check if player succeeded in landing a hit
+    if (attackRoll >= 10) {
         // Dice roll for the attack strength (aka damage)    
         const attackDmg = Math.floor(Math.random() * 11)
-        //Message to on hit
+        //Message on hit
         document.getElementById(`combatLog`).innerHTML = `Your hit lands! You did ${attackDmg} points of damage!`;
         // Damage applied to the Zombie Health Indicator
         let zHealthAfterHit = currentZHealth - attackDmg;
@@ -64,15 +70,14 @@ function attack() {
         if (currentZHealth - attackDmg < 0) {
             zHealthAfterHit = 0;
         }
-
+        // Apply damage to the Zombie Health Indicator by reducing it's width by the dmg done
         document.getElementById(`zHealthIndicator`).style.width = `${zHealthAfterHit}%`;
         currentZHealth = zHealthAfterHit;
 
     }
     else {
+        // Message on miss
         document.getElementById(`combatLog`).innerHTML = `Oh no! You missed`;
     }
-
-    // Dice roll for the attack strength (aka damage)
-
 }
+
